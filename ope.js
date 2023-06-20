3dd96044a3bf438309e410be8a5dd9f821c168c5
sabs2007 <129909796+sabs2007@users.noreply.github.com>
2023-06-19 21:52:58 -0300

    19/06/2023- javascript
    na aula da semana passada nos iniciamos o js , estamos fazendo alguns exercicios para o desenvolvimento do aprendisado assim aprendendo cada vez mais. boa noite :)

---
+++ ope.js
@@ -0,0 +1,37 @@
+/*const num1 =100;
+const num2 =500;
+
+soma = num1+num2;
+
+document.write(soma);*/
+
+/*const n1=50;
+const n2=80;
+const n3=60;
+const n4=5;
+
+media = (n1 + n2 + n3 + n4) /4 ;
+
+console.log("A media é",media);
+
+if (media>=70) {
+    console.log("aluno aprovado")
+} else{
+    console.log("aluno reprovado")
+}
+*/
+
+/*const nome=" sabrina";
+const idade= 15;
+
+
+console.log("meu nome é " + nome,"e minha idede é " + idade)
+*/
+
+/*const nome=" sabrina";
+const a= 1.70;
+const p=83;
+
+imc =  p /(a*a) ;
+
+console.log("meu nome é " + nome, "e meu IMC é " + imc);*/
