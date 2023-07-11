01e539dbf3c0a248f4d494c3b5c90ffe4d74c7eb
sabs2007 <129909796+sabs2007@users.noreply.github.com>
2023-07-10 21:53:44 -0300

    10/07/2023- praticas
    boa noite

--- soma.js
+++ soma.js
@@ -1,51 +1,12 @@
-/*let idade = 70;
+const resultado = document.getElementById("resultado")
 
-if(idade<= 10 ){
-    console.log("essa pessoa é uma criança");
-}else if(idade>= 17 && idade <=17){
-    console.log("Essas pessoa é um adolecente");
-}else if(idade >=18 && idade <=60){
-    console.log("essa pessoa é adulta");
-}else {
-    console.log("essa pessoa é idosa");
-}**/
+resultado.addEventListener("click", function soma(){
+    const num1 = parseInt(document.getElementById
+        ("numero1").value);
+    const num2 = parseInt(document.getElementById
+     ("numero2").value);
+     
+    let total = document.getElementById("total");
 
-/*const nota1 = 4;
-const nota2 =3;
-const nota3 =3;
-const nota4 =3;
-
-media = (nota1 + nota2 + nota3 + nota4) /4;
-
-console.log("A media é", media)
-
-if (media <=3.99) {
-    console.log("Aluno reprovado");
-}
-else if(media >=4 && media <=6.99){
-    console.log("Aluno de recuperação");
-}
-else{
-    console.log("Aluno aprovado");}*/
-
-let p=83;
-let a=1.80;
-let imc = p/(a*a);
-
-console.log("meu imc é" , imc);
-
-if(imc < 18.5){
-    console.log("abaixo do peso");
-}
-else if (imc >=18.5 && imc <25 ) {
-    console.log("peso normal");
-}
-else if ( imc >=25 && imc <30 ) {
-    console.log("acima do peso");
-}
-else if (imc >=30 && imc <40){
-    console.log("obeso");
-}
-else{
-    console.log("obesidade grave");
-}
+    total.textContent = num1 + num2;
+})
--- tabela.js
+++ tabela.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2){
     return`
     <li>
-    <img src="./icon-${player1}.png"alt="bandeira do ${player1}"/>
+    <img src="times1/icon-${player1}.png"alt="bandeira do ${player1}"/>
     <strong>${hour}</strong>
-    <img src="./icon-${player2}.png"alt="bandeira do ${player2}"/>
+    <img src="times1/icon-${player2}.png"alt="bandeira do ${player2}"/>
     `
 }
 let delay= -0.4;
@@ -19,4 +19,4 @@ function creatCard(date, day, games){
     `
 }
 document.querySelector("#cards").innerHTML=
-creatCard("05/07", "quarta-feira", createGame("corinthias", "21:00", "palmeiras"))
+creatCard("05/07", "quarta-feira", createGame("corinthians", "21:00", "palmeiras"))+creatCard("10/07", "segunda-feira", createGame("atletico", "20:00", "bahia"))+creatCard("12/07", "quarta-feira", createGame("SP", "21:00", "vasco"))+creatCard("14/07", "sextA-feira", createGame("inter", "18:00", "santos"))+creatCard("16/07", "domingo", createGame("bota", "16:00", "flamengo"))+creatCard("20/07", "quinta-feira", createGame("cruzeiro", "21:00", "gremio"))
